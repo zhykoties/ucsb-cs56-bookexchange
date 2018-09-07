02aaa3e175af62699b4e3606cde17d8cf2e1e5e6
Yunkai Zhang <zhykoties@gmail.com>
2018-09-06 23:48:55 -0700

    "set up"

--- src/main/java/edu/ucsb/cs56/pconrad/SparkDemo01.java
+++ src/main/java/edu/ucsb/cs56/pconrad/SparkDemo01.java
@@ -12,20 +12,24 @@ public class SparkDemo01 {
     public static void main(String[] args) {
 
         port(getHerokuAssignedPort());
-		
+
 		System.out.println("");
 		System.out.println("(Don't worry about the warnings below about SLF4J... we'll deal with those later)");
-		System.out.println("");						  
+		System.out.println("");
 		System.out.println("In browser, visit: http://localhost:" + getHerokuAssignedPort() + "/hello");
 		System.out.println("");
-        spark.Spark.get("/", (req, res) -> "Hello World");
+    String html = "<h1><a href='/hello'>Hello</a> World!</h1>\n" +
+    "<p>This web app is powered by \n" +
+    "<a href='https://github.com/zhykoties/sparkjava-01'>this github repo</a></p>\n";
+    spark.Spark.get("/", (req, res) -> html);
+    spark.Spark.get("/hello", (req, res) -> "<p><b>Hello, World!</b>  You just clicked the first link on my web app.</p>");
 		spark.Spark.get("/hello", (req, res) -> "Hello World");
 		spark.Spark.get("/bye", (req, res) -> "Goodbye World");
 		spark.Spark.get("/yo", (req, res) -> "S'up World");
-		spark.Spark.get("/tension", (req, res) -> "Midterm next week.  No problem dude");		
+		spark.Spark.get("/tension", (req, res) -> "Midterm next week.  No problem dude");
 
 	}
-	
+
     static int getHerokuAssignedPort() {
         ProcessBuilder processBuilder = new ProcessBuilder();
         if (processBuilder.environment().get("PORT") != null) {
@@ -34,5 +38,5 @@ public class SparkDemo01 {
         return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 
-	
+
 }
